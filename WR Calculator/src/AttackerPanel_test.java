6fe04b4a84b18baf55d30588ccedbfb53b1e2b5d
GenSan9097 <58377492+GenSan9097@users.noreply.github.com>
2020-10-08 17:06:50 +0900

    DBManager,AttackerPanel created

---
+++ WR Calculator/src/AttackerPanel_test.java
@@ -0,0 +1,48 @@
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class AttackerPanel_test extends JPanel {
+	
+	public JPanel panel_botAndWeapon;
+	public JPanel panel_buff;
+	public JPanel panel_module;
+	public JPanel panel_pilot;
+	public JPanel panel_drone;
+	
+	public JLabel label_bot;
+	public JLabel label_weapon1;
+	public JLabel label_weapon2;
+	public JLabel label_weapon3;
+	public JLabel label_weapon4;
+	public JComboBox combo_bot;
+	public JComboBox combo_weapon1;
+	public JComboBox combo_weapon2;
+	public JComboBox combo_weapon3;
+	public JComboBox combo_weapon4;
+	
+	public JLabel label_you;
+	public JLabel label_sqMem;
+	public JCheckBox cbox_you;
+	public JTextField field_sqMem;
+	public JLabel label_totalBuff;
+	
+	public JLabel label_overdrive;
+	public JLabel label_thermonuke;
+	public JLabel label_cheapThermo;
+	public JLabel label_deathmark;
+	public JFormattedTextField format_overdrive;
+	public JFormattedTextField format_thermonuke;
+	public JFormattedTextField format_cheapuThermo;
+	public JCheckBox cbox_deathmark;
+	
+	
+	
+	public AttackerPanel_test() {
+		super();
+	}
+	
+}
---
+++ WR Calculator/src/DBManager.java
@@ -0,0 +1,11 @@
+
+public class DBManager {
+	public static final String DBNAME = "wr_calc";
+	public static final String USER = "wr_calc_user";
+	public static final String PASSWORD = "warrobots";
+	public static final String JDBCDriver = "PostgresJDBCDriver";
+	public static final String DBURL = "jdbc:postgresql://localhost/" + DBNAME;
+	
+	
+	
+}
